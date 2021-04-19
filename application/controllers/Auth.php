d18218c02d9bb9dea55d5054116151a0db4da7f9
InaBinti Abdul Shukor <ina.abdulshukor@usm.my>
2021-04-19 15:17:44 +0800

    template registration and login page

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,6 +5,15 @@ class Auth extends CI_Controller
 {
     public function index()
     {
-        echo 'index';
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+    public function registration()
+    {
+
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
     }
 }
