4164cd5e35044c13afcbb6ad3bff7fd1990d2634
InaBinti Abdul Shukor <ina.abdulshukor@usm.my>
2021-04-20 13:37:07 +0800

    registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,18 +3,37 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Login';
+
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
     public function registration()
     {
-        $data['title'] = 'User Registration';
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
 
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+
+        if ($this->form_validation->run() == false) {
+
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo "successful login";
+        }
     }
 }
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -12,35 +12,38 @@
                         <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
 
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name">
+                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name'); ?>">
+                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
-                                </div>
-                                <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-user btn-block">
-                                Register Account
-                            </button>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                        </div>
+                        <div class="col-sm-6">
+                            <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                        Register Account
+                    </button>
 
-                        </form>
-                        <hr>
-                        <div class="text-center">
-                            <a class="small" href="forgot-password.html">Forgot Password?</a>
-                        </div>
-                        <div class="text-center">
-                            <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
-                        </div>
+                    </form>
+                    <hr>
+                    <div class="text-center">
+                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    </div>
+                    <div class="text-center">
+                        <a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 </div>
